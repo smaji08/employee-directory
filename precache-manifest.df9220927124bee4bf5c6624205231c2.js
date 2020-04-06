64c7ed6769bd4f4b66aebec88d9c64b47de4b112
Augustine <smaji08@gmailcom>
2020-04-06 14:08:46 -0400

    Updates

---
+++ precache-manifest.df9220927124bee4bf5c6624205231c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17849d7861017e0bc25dc01dc999e4c9",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "e17ef25081a790873ee0",
+    "url": "/employee-directory/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "4fb748e48f11e28d3044",
+    "url": "/employee-directory/static/js/2.101a7766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.101a7766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17ef25081a790873ee0",
+    "url": "/employee-directory/static/js/main.ba4b01e2.chunk.js"
+  },
+  {
+    "revision": "1555ac21ead60afe404d",
+    "url": "/employee-directory/static/js/runtime-main.2264778b.js"
+  }
+]);
